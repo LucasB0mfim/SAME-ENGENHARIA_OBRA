49da69fecd083b888f38ea071854004ddce8b93d
LucasB0mfim <lucasbomfimsobral@gmail.com>
2025-08-15 15:22:41 -0300

    style: Gerar estilo

--- src/app/components/new-task/new-task.component.ts
+++ src/app/components/new-task/new-task.component.ts
@@ -11,9 +11,72 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class NewTaskComponent {
 
-  private readonly _route = inject(Router)
+  private readonly _route = inject(Router);
 
-  onReturn(): void {
+  stepOne: boolean = true;
+  stepTwo: boolean = false;
+  stepThree: boolean = false;
+  stepFour: boolean = false;
+  stepFive: boolean = false;
+
+  onHome(): void {
     this._route.navigate(['dashboard']);
   }
+
+  onStepOne(): void {
+    this.stepOne = true;
+    this.stepTwo = false;
+    this.stepThree = false;
+    this.stepFour = false;
+    this.stepFive = false;
+  }
+
+  onStepTwo(): void {
+    this.stepOne = false;
+    this.stepTwo = true;
+    this.stepThree = false;
+    this.stepFour = false;
+    this.stepFive = false;
+  }
+
+  onStepThree(): void {
+    this.stepOne = false;
+    this.stepTwo = false;
+    this.stepThree = true;
+    this.stepFour = false;
+    this.stepFive = false;
+  }
+
+  onStepFour(): void {
+    this.stepOne = false;
+    this.stepTwo = false;
+    this.stepThree = false;
+    this.stepFour = true;
+    this.stepFive = false;
+  }
+
+  onStepFive(): void {
+    this.stepOne = false;
+    this.stepTwo = false;
+    this.stepThree = false;
+    this.stepFour = false;
+    this.stepFive = true;
+  }
+
+  formatCurrency(event: any) {
+    let value = event.target.value;
+
+    // Remove tudo que não é dígito
+    value = value.replace(/\D/g, "");
+
+    // Converte para número e divide por 100
+    const numericValue = parseFloat(value) / 100;
+
+    // Formata como BRL
+    event.target.value = numericValue.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    });
+  }
+
 }
